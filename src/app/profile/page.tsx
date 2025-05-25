b771ac5a522b1988be73a8cc81ef143d1c90d586
Azzurriii <vtthanh04.qb@gmail.com>
2025-05-25 08:11:49 +0700

    feat(wallet): redeem

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -64,7 +64,7 @@ export default function ProfilePage() {
         walletAddress={address}
         isConnecting={connecting}
       />
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="container mx-auto px-6 pt-32 pb-20">
         <ProfileHeader />
         <ProfileTabs />
       </div>
